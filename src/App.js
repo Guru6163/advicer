15f81038b0e056209948d5311f38b3c310c23583
Guruprasath <guruprasath@jarvisml.com>
2023-02-03 23:56:47 +0530

    Advice Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [advice, setAdvice] = useState("")
+  const [disabled, setDisabled] = useState(false)
+
+  const fetchAdvice = () => {
+    setDisabled(true)
+    setAdvice("Loading.... :)")
+    fetch("https://api.adviceslip.com/advice")
+      .then(res => res.json())
+      .then(res => setAdvice(res.slip.advice))
+      .then(res => setTimeout(() => setDisabled(false), 1000))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='card'>
+        <h2 className='heading'>{advice ? advice : "Hey, you looks like demotivated, get a advice from me now"}</h2>
+        {!disabled && <button className='button' disabled={disabled} onClick={() => fetchAdvice()}>Give me a Advice</button>}
+
+      </div>
+
+
     </div>
   );
 }
